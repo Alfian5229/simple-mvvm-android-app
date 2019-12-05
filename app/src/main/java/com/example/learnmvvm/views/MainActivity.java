5cca9c268ed30a99ad3720d541b16e8c72a91b36
alfianrehanusa <wibowoalfian5229@gmail.com>
2019-12-05 16:31:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/learnmvvm/views/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.learnmvvm.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.learnmvvm.R;
+import com.example.learnmvvm.databinding.ActivityMainBinding;
+import com.example.learnmvvm.viewmodels.LoginViewModel;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
